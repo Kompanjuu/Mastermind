b0ee9a95c7c2392316c3e44d61074459583fa9bb
Kseniya Khafizovanagornykh <kseniya.khafizovanagornykh@skola.taby.se>
2025-11-06 14:21:20 +0000

    påbörjade. försöker göra input till lista //Kseniya

---
+++ puh.py
@@ -0,0 +1,19 @@
+#make this a fucntion (def sjkndasjk()
+    print("Hewo Pwincess UwU \n Du ska ange en gissning som följd av fyra siffror!")
+    numb = input("Ange fyra siffror mellan och inklusive 1-6:")
+    replaced = numb.replace(" ","")
+    while True
+        #take an input
+        length = len(replaced) #chheck length
+        if length == 4:
+            #turn the thing into a list and make it numbers (int)
+            #check every number in the list and make sure its within the 1-6 range
+            #once finished return answer
+        else:
+            #let the while loop comtinue
+
+
+#    print ("error, too many numbers")
+#elif l < 1:
+#    print ("error, too little numbers")
+#else:
