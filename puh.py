3d93616e1062a9262d48191214e2a99dc786aa47
Lekitio <hanna.chen.citron@gmail.com>
2025-11-13 14:34:40 +0100

    it should be fucntional now, change messge so that it gives all advice at once not if you pass one filter and not the other, also make the comment nicer. Wrong isnt very user frieldy. //Hanna

--- puh.py
+++ puh.py
@@ -1,36 +1,52 @@
+
+
+
 def game_start ():
     print("Hewo Pwincess UwU \n Du ska ange en gissning som följd av fyra siffror!")
-    numb = input("Ange fyra siffror mellan och inklusive 1-6:")
-    replaced = numb.replace(" ","")
-    #Denna biten printar först ut ett välkomst medelande som också get en kort förklaring av vad spelaren ska göra. Därefter defineras variabel "numb" och man får ange siffror.
-    #Till sist byter den ut alla mellanslag som spelaren skrev in till inget.
+    
 
-    while True:
-        lst = []
-        #take an input
-        length = len(replaced) #chheck length
-        if length != 4:
+    while True: #contantly try to take an input until proper input is inputted.
+        numb = input("Ange fyra siffror mellan och inklusive 1-6: ")
+        numb = numb.replace(" ","")
+        #Denna biten printar först ut ett välkomst medelande som också get en kort förklaring av vad spelaren ska göra. Därefter defineras variabel "numb" och man får ange siffror.
+        #Till sist byter den ut alla mellanslag som spelaren skrev in till inget.
+        
+        #now we have a string with no spaces. Length? Number?
+
+        numb_lst = []
+        length = len(numb) #check length
+        retry = False
+        if length != 4: #this filters out all thats not length 4
             print("Wrong")
             continue
-    #En while loop påbörjas. Loopen definerar först variabel "lst". Därefter checkar den om de nummer spelaren skrev in i inputen har exakt 4 siffror.
-    #Om det inte är 4 siffror printar den ut "wrong" och man får skriva in några siffror igen tills det är 4 st siffror och koden fortsätter till nästa steg med "continue".
+        #En while loop påbörjas. Loopen definerar först variabel "lst". Därefter checkar den om de nummer spelaren skrev in i inputen har exakt 4 siffror.
+        #Om det inte är 4 siffror printar den ut "wrong" och man får skriva in några siffror igen tills det är 4 st siffror och koden fortsätter till nästa steg med "continue".
+        #continue continues the loop. it goes onto the next loop.
 
-        for num in replaced:
+        for num in numb: #is every individual "character" in the string once. ABCD -> A -> B -> C -> D
             try:
-                num = int(num)
-                if num > 6 or num < 1:
-                    print("Try again, too big or too small")
-                    break
-                lst.append(num)
-                #append add smoethign to a list
-                #[].append(1)
-                #[1]
-
+                num = int(num) #filters out if you inputed a bokstav
+                #This will try turning the num into an integer.
+                #this will fail is num is not a heltal (0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11....)
+                numb_lst.append(num)
             except ValueError: #this means they answered a bokstav nto number
-                print("Try again")
-        if len(lst) == 4:
-            break
-    return lst
+                print("Try again, you need numebrs")
+                retry = True
+                break
+
+
+        for num in numb_lst: #this is a list of 4 integers (heltal). We want to chack the range.
+            if num > 6 or num < 1: #Another filter. we check if its withtin the range that we allow (1 to 6)
+                print("Try again, too big or too small")
+                retry = True
+                break
+            #append add smoethign to a list
+            #[].append(1)
+            #[1]
+        if retry: # = True
+            continue
+        break #if it psses all of our filters.
+    return numb_lst
 
 print(game_start())
         #Biten här påbörjar en For-loop.
