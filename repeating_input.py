11c801c658294c7cdc58dc9a45c771a13fa6a84a
Kseniya Khafizovanagornykh <kseniya.khafizovanagornykh@skola.taby.se>
2025-11-25 07:46:09 +0000

    fixade utseende lite. Tog bort onödiga kommentarer. //Kseniya

--- repeating_input.py
+++ repeating_input.py
@@ -3,7 +3,7 @@ import messages
 
 def game_start ():
     txt = "│Hej spelare!│🖑"
-    
+
     print("\n", txt.center(65), sep = "")
     print("\t ☆ Du ska ange en gissning som följd av fyra siffror!")
 
@@ -18,40 +18,39 @@ def game_start ():
         #now we have a string with no spaces. Length? Number?
 
         numb_lst = []
-        length = len(numb) #check length
+        length = len(numb)
         retry = False
-        if length != 4: #this filters out all thats not length 4
+        if length != 4: #This filters out all thats not length 4
             error_message.append(messages.length_not_four)
-            # continue, instead of directly going to the next loop lets collect everything thats wrong with the lessage
+
         #En while loop påbörjas. Loopen definerar först variabel "lst". Därefter checkar den om de nummer spelaren skrev in i inputen har exakt 4 siffror.
-        #Om det inte är 4 siffror printar den ut "wrong" och man får skriva in några siffror igen tills det är 4 st siffror och koden fortsätter till nästa steg med "continue".
-        #continue continues the loop. it goes onto the next loop.
+        #Om det inte är 4 siffror printar den ut en error message och man får skriva in några siffror igen tills det är 4 num.
 
         for num in numb: #is every individual "character" in the string once. ABCD -> A -> B -> C -> D
             try:
-                num = int(num) #filters out if you inputed a bokstav
+                num = int(num) #Filters out if you inputed a bokstav
                 #This will try turning the num into an integer.
-                #this will fail is num is not a heltal (0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11....)
+                #This will fail if num is not a heltal (0, 1, 2, 4, 5, 6, 7, 8, 9, 10....)
                 numb_lst.append(num)
-            except ValueError: #this means they answered a bokstav nto number
+            except ValueError: #This means they answered a letter instead of a number
                 error_message.append(messages.not_integers)
                 break
 
 
-        for num in numb_lst: #this is a list of 4 integers (heltal). We want to chack the range.
+        for num in numb_lst: #This is a list of 4 integers (heltal). We want to chack the range.
             if num > 6 or num < 1: #Another filter. we check if its withtin the range that we allow (1 to 6)
                 error_message.append(messages.int_not_within_range)
                 break
-            #append add smoethign to a list
+            #Append adds smomething to a list
             #[].append(1)
             #[1]
-        if len(error_message) > 0: #this is a collected place where all the errors gets sent./Hanna
+        if len(error_message) > 0: #This is a collected place where all the errors gets sent./Hanna
             error_message = list(map(lambda x: " ☆ " + x, error_message))#this adds a "☆ " before each message. Cleaner looking
-            print("\nSecret hints just for you ;):" ,"\n\t","\n\t".join(error_message),"\n", sep="") #this is where the message gets send out
+            print("\nSecret hints just for you ;):" ,"\n\t","\n\t".join(error_message), sep="") #This is where the message gets sent out
             continue
-        break #if it psses all of our filters.
+        break #If it passes all filters.
     return numb_lst
-# print(game_start())
+#print(game_start())
         #Biten här påbörjar en For-loop.
         #Eftersom nummrerna spelaren skrev in räknas som en string, skriver man en kod för att omvandla stringen till integer.
         #Den checkar om nummrerna spelaren skrev in är nummer mellan och inklusive 1-6
@@ -59,7 +58,3 @@ def game_start ():
         #Koden ger en error om man har skrivit en bokstav och printar ett medelande
 
 
-#    print ("error, too many numbers")
-#elif l < 1:
-#    print ("error, too little numbers")
-#else:
